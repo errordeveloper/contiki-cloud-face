76b4a133b987b122837fdc1ef80fed4822ef1c88
Ilya Dmitrichenko <ilya@ultraspeed.com>
2011-11-07 16:37:18 +0000

    Now using `google.load()` and the visualisation API.

--- lib_ui/jqui.js
+++ lib_ui/jqui.js
@@ -1,3 +1,6 @@
+google.load('visualization', '1', {packages:['table']});
+google.load('jquery', '1.6.4');
+
 function sel_ui (ui_class) {
   var classes = {
     // TODO: Implement shorthand
@@ -12,27 +15,48 @@ function sel_ui (ui_class) {
          */
         'n': function(v) {
           $('.main').append('<h3>Neighbourhood Nodes:</h3>');
-          $('.main').append('<ul id=\"nodes\"></ul>');
+          $('.main').append('<div id=\"nodes\"></div>');
 
-          $.each(v, function (i, x) { if (x.toString() !== '') {
+	  var data = new google.visualization.DataTable();
 
-              $('#nodes').append('<li id=\"node_'+i+'\"><pre>'+x+'</pre></li>');
+	  /*
+	  data.addColumn('string', 'a string');
+	  data.addColumn('number', 'a number');
+	  data.addColumn('boolean', 'on/off');
+	  */
 
-          }});
+	  data.addColumn('string', 'IP address');
+	  data.addRows(v.length-1);
 
+          $.each(v, function (i, x) { if (x.toString() !== '') { data.setCell(i,0,x); }});
+
+	  var table = new google.visualization.Table(document.getElementById('nodes'));
+	  table.draw(data, {showRowNumber: true});
         },
         'r': function(v) {
           /* XXX: what can be done with the routes here? Any graphing needed? */
           $('.main').append('<h3>Available Routes:</h3>');
-          $('.main').append('<ol id=\"routes\"></ol>');
+          $('.main').append('<div id=\"routes\"></div>');
+
+	  var data = new google.visualization.DataTable();
+	  data.addColumn('string', 'Gateway IP address');
+	  data.addColumn('string', 'Next hop IP address');
+	  data.addColumn('number', 'TTL');
+	  data.addRows(v.length-1);
 
           //$('#route').append('<li><pre>'+v.toString+'</pre></li>');
           $.each(v, function (i, x) { if (x.toString() !== '') {
 
-            var s = x[0]+'/'+x[1].toString()+' via '+x[2]+' ('+x[3]+'s)';
-            $('#routes').append('<li><pre>'+s+'</pre></li>');
+	    data.setCell(i,0,x[0]+'/'+x[1]);
+	    data.setCell(i,1,x[2]);
+	    data.setCell(i,2,x[3]);
+            //var s = x[0]+'/'+x[1].toString()+' via '+x[2]+' ('+x[3]+'s)';
+            //$('#routes').append('<li><pre>'+s+'</pre></li>');
 
           }});
+
+	  var table = new google.visualization.Table(document.getElementById('routes'));
+	  table.draw(data, {showRowNumber: true});
 
         }
       //}
@@ -59,10 +83,3 @@ function gen_ui (ui_class, params) {
     }
   }
 };
-
-/*
-$(document).ready(function() {
-  var $d = $('<div class="main"/>');
-  $('body').append($d);
-});
-*/
