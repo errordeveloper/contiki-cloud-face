29d81a7b4c47b33dfb7db3db26ff2f9cf42da51a
Ilya Dmitrichenko <errordeveloper@gmail.com>
2011-11-09 01:11:13 +0000

    Started working on Pachube feed creation form ...

---
+++ lib_ui/pachube-misc/example-feed.js
@@ -0,0 +1,64 @@
+{
+  "title" : "Pachube Office environment",
+  "description" : "test of manual feed snapshotting",
+  "feed" : "http://api.pachube.com/v2/feeds/504.json",
+  "id" : 504,
+  "status" : "frozen",
+  "website" : "http://www.haque.co.uk/",
+  "updated" : "2010-06-25T11:54:17.463771Z",
+  "version" : "1.0.0",
+  "private" : "false",
+  "creator" : "http://www.pachube.com/users/hdr",
+  "location":
+    {
+      "disposition":"fixed",
+      "ele":"23.0",
+      "name":"office",
+      "lat":51.5235375648154,
+      "exposure":"indoor",
+      "lon":-0.0807666778564453,
+      "domain":"physical"
+    },
+  "tags" : [
+    "Tag1",
+    "Tag2"
+  ],
+  "datastreams" : [
+    {
+      "at" : "2010-06-25T11:54:17.454020Z",
+      "current_value" : "999",
+      "datapoints" : [
+        {
+          "at" : "2005-06-25T11:54:17.000000Z",
+          "value" : "999"
+        },
+        {
+          "at" : "2006-06-25T11:54:17.000000Z",
+          "value" : "123"
+        }
+      ],
+      "id" : "3",
+      "max_value" : "999.0",
+      "min_value" : "7.0",
+      "unit": {
+          "type": "conversionBasedUnits",
+          "label": "label",
+          "symbol": "symbol"
+      },
+    },
+    {
+      "at" : "2010-06-24T10:05:49.000000Z",
+      "current_value" : "0000017",
+      "datapoints" : [
+        {
+          "at" : "2010-04-12T11:31:51.133782Z",
+          "value" : "999"
+        }
+      ],
+      "id" : "4",
+      "max_value" : "19.0",
+      "min_value" : "7.0"
+    }
+  ]
+}
+
